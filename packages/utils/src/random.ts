39103660cb7302d699dccaebd3b922e119fb6900
dennisja <dennisjjagwe@gmail.com>
2021-02-05 19:10:21 +0100

    chore(linting) add linting

--- packages/utils/src/random.ts
+++ packages/utils/src/random.ts
@@ -2,6 +2,6 @@
  * Get a random item from an array of items
  * @param array The array of items
  */
-export const getRadomItem = <T>(array: T[]) => {
+export const getRadomItem = <T>(array: T[]): T => {
   return array[Math.trunc(Math.random() * array.length)];
 };
