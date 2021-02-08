8eb1d3d4ddba68f7f677673146b1e29320b12d3c
dennisja <dennisjjagwe@gmail.com>
2021-02-08 16:34:13 +0100

    fix(utils): conventional commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ["@commitlint/config-lerna-scopes"] };
+module.exports = {
+  extends: [
+    /** prevent us from making commits that don't conform to the convention */
+    "@commitlint/config-conventional",
+
+    /** understand what lerna scopes are */
+    "@commitlint/config-lerna-scopes",
+  ],
+};
